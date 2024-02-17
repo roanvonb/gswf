92ecf67c6880e11321d42902afce0da12be0f68f
Roan <roanvonb@gmail.com>
2024-02-18 09:50:51 +1030

    terrain and ion kml fetch working together

--- src/components/ResiumViewer.jsx
+++ src/components/ResiumViewer.jsx
@@ -1,4 +1,5 @@
 import * as Cesium from 'cesium';
+import { useEffect, useMemo, useRef } from 'react';
 import {
     CameraFlyTo,
     Entity,
@@ -13,10 +14,51 @@ Cesium.Ion.defaultAccessToken =
 const default_compound_position = Cesium.Cartesian3.fromDegrees(138.910467565, -33.804836895);
 const default_camera_position = Cesium.Cartesian3.fromDegrees(138.95, -33.77, 25000);
 
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const fetchTerrain = async (ref) => {
+    if (ref.current && ref.current.cesiumElement) {
+        ref.current.cesiumElement.terrainProvider = await Cesium.createWorldTerrainAsync()
+
+    }
+    else {
+        await sleep(1000);
+        fetchTerrain(ref);
+    }
+
+}
+
+const fetchKml = async (ref) => {
+    if (ref.current && ref.current.cesiumElement) {
+        const url = await Cesium.IonResource.fromAssetId(2462247);
+        const kml = await Cesium.KmlDataSource.load(url, { clampToGround: true });
+        ref.current?.cesiumElement.dataSources.add(kml);
+    }
+    else {
+        await sleep(1000);
+        fetchKml(ref);
+    }
+
+}
+
+
+
 export function ResiumViewer() {
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        fetchTerrain(ref);
+        fetchKml(ref);
+    }, []);
+
+
 
     return (
         <Viewer
+            ref={ref}
             full
             timeline={false}
             animation={false}

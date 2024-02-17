012688a5ab68be46174c4f4eca59380e27198cc3
Roan <roanvonb@gmail.com>
2024-02-17 21:01:14 +1030

    cesium access token as env variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import * as Cesium from 'cesium';
 import './App.css'
 import { Viewer } from 'resium'
 
+Cesium.Ion.defaultAccessToken = import.meta.env.VITE_ION_TOKEN;
+
 function App() {
   return (
-    <Viewer/>
+    <Viewer full />
   )
 }
 

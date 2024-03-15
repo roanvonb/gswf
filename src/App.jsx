05bcf7ca2eab2aa238ee9a748a68984d74807b32
Roan Von Benecke <rovonbenecke@alexander.com.au>
2024-03-15 10:48:37 +1030

    installed prettier and mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { Router } from './Router';
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+
+// other css files are required only if
+// you are using components from the corresponding package
+// import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
+// ...
+
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App() {
   return (
-    <Router />
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
   );
 }
